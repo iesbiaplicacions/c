c113d0eadcb60f33aa512c6418e924c3ad62d000
iesbiaplicacions <94534894+iesbiaplicacions@users.noreply.github.com>
2025-09-27 12:33:44 +0200

    Add files via upload

---
+++ hola.c
@@ -0,0 +1,10 @@
+/******************************
+  Programa Hola món en C
+  Xavi Blanes 1r bat curs 25-26
+******************************/
+
+#include <stdio.h>
+void main(){
+    printf("Hola món\n");
+return;
+}
