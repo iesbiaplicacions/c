a112576c99c44cb0842c4208b57927451796cbf4
iesbiaplicacions <94534894+iesbiaplicacions@users.noreply.github.com>
2025-10-01 13:57:37 +0200

    Create mitjana2.c

---
+++ mitjana2.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+void main(){
+    int a, b, mitjana;
+    printf("1r núm?: ");
+    scanf("%d", &a);
+    printf("2n núm?: ");
+    scanf("%d", &b);
+    printf("La mitjana és: %d", (a+b)/2);
+    return;
+}
