ad8a62669119d494000d204e454814489f2adef5
iesbiaplicacions <94534894+iesbiaplicacions@users.noreply.github.com>
2026-03-12 21:23:02 +0100

    Fix comment formatting in 5x5SumaFila.c

--- 5x5SumaFila.c
+++ 5x5SumaFila.c
@@ -1,9 +1,8 @@
 /****************************************************************
- * Matriu de 5x5 d'enters aleatoris amb la 5a columna com a suma de la /                   *
+ * Matriu de 5x5 d'enters aleatoris amb la 5a columna com a     *
+ * suma dels elements de la fila                                *
  * Xavi Blanes curs: 25/26                                      *
- * Completa el codi...                                          *
- * *************************************************************/
-
+ ***************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
