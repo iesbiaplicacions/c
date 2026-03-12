dfc6e9fe975bef75c0cf57276785de4f21f071ad
iesbiaplicacions <94534894+iesbiaplicacions@users.noreply.github.com>
2026-03-12 21:26:10 +0100

    Add header comment for 5x5alea.c

--- 5x5alea.c
+++ 5x5alea.c
@@ -1,3 +1,8 @@
+/****************************************************************
+ * Matriu de 5x5 d'enters aleatoris                             *
+ * Xavi Blanes curs: 25/26                                      *
+ ***************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
