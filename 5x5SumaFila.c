6cd6f6608e5a775fd3499bf669f4b3fb3bfee723
iesbiaplicacions <94534894+iesbiaplicacions@users.noreply.github.com>
2026-03-12 12:54:52 +0100

    Fix variable declaration and update print message

--- 5x5SumaFila.c
+++ 5x5SumaFila.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main() {
-    i, j, suma;
+    int i, j, suma;
     int matriu[5][5];
     
     // Inicialitzar la llavor per als números aleatoris
@@ -11,13 +11,16 @@ int main() {
 
     // Omplir la matriu amb números aleatoris
     for(i = 0; i < 5; i++) {
+        suma = 0;
         for(j = 0; j < 4; j++) {
             matriu[i][j] = rand() % 100; // números entre 0 i 99
+            suma = suma + matriu[i][j];
         }
+        matriu[i][j] = suma;
     }
 
     // Imprimir la matriu
-    printf("Matriu 5x5 amb numeros aleatoris:\n");
+    printf("Matriu 5x5 amb numeros aleatoris i la suma:\n");
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%3d ", matriu[i][j]);
