25fa9164a5d12dc17568d65f523d993b500b2ee8
iesbiaplicacions <94534894+iesbiaplicacions@users.noreply.github.com>
2026-03-25 15:19:35 +0100

    Fix identitat function to accept 10x10 matrix

--- matrius/identitat.c
+++ matrius/identitat.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int identitat(int m[][], int dimensio);
+int identitat(int m[10][10], int dimensio);
 
 int main() {
     int i, j;
-    int matriu[3][3] = { {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    int matriu[10][10] = { {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
     if(identitat(matriu,3)) printf("Identitat\n");
-    else printf("No identitat\n")
+    else printf("No identitat\n");
     return 0;
 }
 
-int identitat(int m[][], int dimensio){
+int identitat(int m[10][10], int dimensio){
 int i, j, id;
-id = true;
-for(int i = 0; i < dimensio; i++)
-        for(int j = 0; j < dimensio; j++)
+id = 1;
+for(i = 0; i < dimensio; i++)
+        for(j = 0; j < dimensio; j++)
             if(i == j) id = id && (m[i][j] == 1);
             else id = id && (m[i][j] == 0);
 return id;
